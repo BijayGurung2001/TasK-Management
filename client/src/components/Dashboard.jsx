ed80cfdd689b3ba6060601235252fdff0382a410
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-08 22:08:36 +0545

    update in dashboard components

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -8,9 +8,9 @@ useEffect(()=>{
   const fetchdata=async ()=>{
     try {
       const result = await fetch('http://localhost:80/user')
-      const fetchdata= result.json();
+      const fetchdata= await result.json();
       setData(fetchdata)
-      console.log(data)
+      console.log(fetchdata)
     } catch (error) {
       console.log("error occured in fetching dashboard data")
     }
@@ -20,13 +20,13 @@ useEffect(()=>{
 },[])
   return (
     <div>
-        <h2 className='text-5xl mb-10'>User Info</h2>
+        <h2 className='text-5xl m-10 '>User Info</h2>
        {data.map((d, i)=>(
 
         <ul key={i}>
-        <li className='text-5xl'>name:{d.name}</li>
-        <li className='text-5xl'>email:{d.email}</li>
-        <li className='text-5xl'>Address:{d.address}</li>
+        <li className='text-5xl'>Name: {d.name}</li>
+        <li className='text-5xl'>Email: {d.email}</li>
+        <li className='text-5xl'>Address: {d.address}</li>
         
       </ul>
        ))}
