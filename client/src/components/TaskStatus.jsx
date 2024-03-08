c5f116b6616d63ffc6c367d7b164a75c6407d215
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-08 22:09:29 +0545

    update in taskstatus components

--- client/src/components/TaskStatus.jsx
+++ client/src/components/TaskStatus.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const TaskStatus = () => {
@@ -18,11 +19,15 @@ const TaskStatus = () => {
           fetchdata()
         },[])
          
+        const handlecomplete=async(taskid)=>{
+         const response=await axios.post('http://localhost:80/history/history', {taskid: taskid})
+         console.log(response.data)
+        }
        
     
 
   return (
-    <div className='bg-slate-400 mt-10 h-4/5 w-screen grid grid-rows-4 grid-flow-col gap-4 p-5'>
+    <div className='bg-slate-400 p-5 h-4/5 w-screen grid grid-rows-4 grid-flow-col gap-4'>
         
         {data.map((task, index)=>(
     <div key={index} className='w-52 h-8/10 bg-lime-800 p-1 pl-2 mt-5'>
@@ -30,7 +35,7 @@ const TaskStatus = () => {
 <p className='text-black font-sans text-xs'>Deadline: {task.date}</p>
 <p className='text-black font-sans text-xs'>Description: {task.description}</p>
 <p className='text-black font-sans text-xs'>Status: {task.status}</p>
-<button className='text-white bg-lime-600 rounded text-sm p-2 m-2 hover:bg-lime-950 hover:text-base'>Complete</button>
+
     </div>
         ) )}
        
