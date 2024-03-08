5fa8d5cb60393dff88409b1e7b8e23b19d6fa747
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-08 22:09:11 +0545

    task history layout update

--- client/src/components/TaskHistory.jsx
+++ client/src/components/TaskHistory.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+
 const TaskHistory = () => {
 const [data, setData]=useState({})
     useEffect(()=>{
         const fetchdata=async()=>{
             try {
-                const result =await axios.get("http://localhost/history");
-                        setData(result.data)
+                const result =await fetch("http://localhost:80/historydata");
+             const fetchdatat=await result.json();
+             setData(fetchdatat)
             } catch (error) {
                 console.error('Error fetching data: ', error)
             }
-            
-        }
+            }
         fetchdata()
     },[])
   return (
-    <div className='grid-cols-3 '>
-       {data.map((e)=>{
-        return(
-            <div key={e.id}>
-            <h1>Titile: {e.Title}</h1>
-            <p>Date: {e.Date}</p>
-            <p>Description: {e.Description}</p>
-            <p>Status: {e.Status}</p>
-        </div>
-        )
-       })}
+<div className='bg-slate-400 p-5 h-4/5 w-screen grid grid-rows-4 grid-flow-col gap-4'>
         
-        
+        {data.map((task, index)=>(
+    <div key={index} className='w-52 h-8/10 bg-lime-800 p-1 pl-2 mt-5'>
+<h1 className='text-black font-sans text-lg'>Title: {task.title}</h1>
+<p className='text-black font-sans text-xs'>Deadline: {task.date}</p>
+<p className='text-black font-sans text-xs'>Description: {task.description}</p>
+<p className='text-black font-sans text-xs'>Status: {task.status}</p>
+<button className='text-white bg-lime-600 rounded text-sm p-2 m-2 hover:bg-lime-950 hover:text-base ' 
+onClick={()=>handlecomplete(task.id)}
+>Complete</button>
+    </div>
+        ) )}
+       
     </div>
   )
 }
