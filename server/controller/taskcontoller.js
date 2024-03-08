6d096f01ca84a37e2449c4af83d9122606278236
BijayGurung2001 <85681343+BijayGurung2001@users.noreply.github.com>
2024-03-08 22:10:11 +0545

    update in task controller file

--- server/controller/taskcontoller.js
+++ server/controller/taskcontoller.js
@@ -1,6 +1,7 @@
 
 const User=require('../models/User')
 const Task=require('../models/taskmodel')
+const TaskHistory=require('../models/TaskHistory')
 
 exports.dashboard=async(req,res)=>{
 
@@ -28,7 +29,7 @@ exports.taskinput=(req,res)=>{
     const task= new Task({title:data.title, date:data.deadline, description: data.description})
     task.save()
     .then(savedTask=>{
-      console.log("Task data inserted successfully", savedTask);
+      console.log("Task data inserted successfully", );
     })
     .catch(err=>{
       console.log("error occurred in inserting task data", err);
@@ -38,9 +39,43 @@ exports.taskinput=(req,res)=>{
 exports.taskstatus=async(req,res)=>{
   try {
     const result= await Task.find();
+   
     res.status(200).json(result)
   } catch (error) {
     console.error("Error fetching tasks:", error);
     res.status(500).json({ error: "Internal server error" });
   }
+}
+
+exports.taskhistory=async(req,res)=>{
+  try {
+    const taskid=req.body.taskid;
+
+    const task= await Task.findOne({id: taskid});
+         if(!task){
+          return res.status(404).json({error:"Task nott found"})
+         }
+         await Task.deleteOne({_id: taskid})
+
+        const historyEntry=new TaskHistory({
+          title: task.title,
+          date:task.date,
+          description:task.description,
+        })
+         await historyEntry.save();
+           
+  } catch (error) {
+    console.error("Error fetching tasks:", error)
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+exports.history=async(req,res)=>{
+  try {
+    const historydata= await TaskHistory.find()
+         res.status(200).json(historydata);
+  } catch (error) {
+    console.log("error in sending data into history", error)
+    res.status(500).json({ error: "Internal server error" });
+  }
+  
 }
